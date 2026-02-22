77987af5b2ec7facdfbc79233e1feb57edc90a7e
percilator <percilator666@gmail.com>
2026-02-22 06:53:05 -0500

    Created .gitignore

--- web/source/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="container"></div>
-</template>
-
-<style lang="scss" scoped src="./App.scss"></style>
--- web/source/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './vars.scss'
-
-const app = createApp(App);
-
-app.mount('#app');
